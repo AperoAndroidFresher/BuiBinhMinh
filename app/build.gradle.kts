d49360588c7a12f9c0667b40c34a9f38c7f1f854
MinhBB-03 <minhbb@apero.talent.vn>
2025-07-29 15:45:17 +0700

    Convert all image to webp and implement change profile image function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/buibinhminh/ui/profileApp/ProfileImage.kt
@@ -0,0 +1,68 @@
+package com.example.buibinhminh.ui.profileApp
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.buibinhminh.R
+
+@Composable
+fun ProfileImageBox(
+    isEditing: Boolean,
+    currentImageUri: Uri?,
+    onImageChange: (Uri?) -> Unit
+) {
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        onImageChange(uri)
+    }
+
+    Box{
+        Image(
+            painter = if (currentImageUri != null) rememberAsyncImagePainter(currentImageUri) else painterResource(id = R.drawable.meo),
+            contentDescription = "Avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .padding(25.dp)
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        if (isEditing) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_photo_camera_24),
+                contentDescription = "Change Image",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xbb000000))
+                    .padding(7.dp)
+                    .align(Alignment.BottomCenter)
+                    .clickable {
+                        galleryLauncher.launch("image/*")
+                    },
+                tint = Color.White,
+            )
+        }
+    }
+}
